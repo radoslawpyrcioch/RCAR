0cd56dce88530bd188b1e1586ca7cd9df1e8fd46
radozip <radozip18@gmail.com>
2021-07-05 20:51:28 +0200

    Add UnitOfWork

---
+++ RCAR.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using RCAR.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCAR.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Car> Car { get; }
+        IGenericRepository<Member> Member { get; }
+        IGenericRepository<PaymentRecord> PaymentRecord { get; }
+        IGenericRepository<Service> Service { get; }
+        IGenericRepository<User> User { get; }
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ RCAR.Domain/Repositories/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using RCAR.Domain.Context;
+using RCAR.Domain.Entities;
+using RCAR.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCAR.Domain.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        private bool disposed = false;
+
+        public UnitOfWork(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            Car = new GenericRepository<Car>(_applicationDbContext);
+            Member = new GenericRepository<Member>(_applicationDbContext);
+            PaymentRecord = new GenericRepository<PaymentRecord>(_applicationDbContext);
+            Service = new GenericRepository<Service>(_applicationDbContext);
+            User = new GenericRepository<User>(_applicationDbContext);
+        }
+        public IGenericRepository<Car> Car { get; private set; }
+        public IGenericRepository<Member> Member { get; private set; }
+        public IGenericRepository<PaymentRecord> PaymentRecord { get; private set; }
+        public IGenericRepository<Service> Service { get; private set; }
+        public IGenericRepository<User> User { get; private set; }
+
+        public void Dispose()
+        {
+            if(!disposed)
+            {
+                _applicationDbContext.Dispose();
+            }
+            disposed = true;
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _applicationDbContext.SaveChangesAsync() > 0);
+        }
+    }
+}
