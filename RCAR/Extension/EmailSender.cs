ddb1e5f06e0108c2b5bc74c3309a8574a17bef1e
radozip <radozip18@gmail.com>
2021-07-05 21:58:38 +0200

    Add EmailSender Helper

---
+++ RCAR/Extension/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace RCAR.Extension
+{
+    public class EmailSender
+    {
+        public static async Task<bool> MailSender(string userEmail, string subject, string body)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+
+                message.To.Add(userEmail);
+                message.From = new MailAddress("testprogramcar1992@gmail.com", "Aplikacja RCarManager");
+                message.IsBodyHtml = true;
+                message.Subject = subject;
+                message.Body = body;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "testprogramcar1992@gmail.com",
+                        Password = "Start123!"
+                    };
+
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
