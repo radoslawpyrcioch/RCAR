e3324fbb4ae261ad3f0c1740d153b20f4264bcb8
radozip <radozip18@gmail.com>
2021-08-05 21:52:48 +0200

    Add Delete script in paymentRecords

---
+++ RCAR/wwwroot/js/delete-payment.js
@@ -0,0 +1,20 @@
+﻿function RemoveFromTable(paymentRecordId) {
+    var result = confirm("Czy na pewno chcesz usuńąć ten rekord?");
+    if (!result)
+        return;
+    else {
+        $.ajax({
+            type: "GET",
+            url: "/PaymentRecord/Delete",
+            data: { id: paymentRecordId },
+            contentType: "application/json; charset=utf-8",
+            dataType: "text",
+            success: function () {
+                location.reload(true);
+            },
+            error: function (response) {
+                alert(response);
+            }
+        })
+    }
+}
