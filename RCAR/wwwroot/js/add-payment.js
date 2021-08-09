39a71b70cb915dcc6486744b9b530de2e04d7834
radozip <pyrcioch.radoslaw@gmail.com>
2021-08-09 22:04:50 +0200

    Add Create script in Index Service

--- RCAR/wwwroot/js/add-payment.js
+++ RCAR/wwwroot/js/add-payment.js
@@ -1,18 +1,25 @@
-﻿function openModal () {
-    $("#exampleModalCenter").modal('show');
-}
-
-function createService() {
-
-    var data = $("#myform").serialize();
-
-    $ajax({
-        type: "post",
-        url: "/service/create",
-        data: data,
-        success: function (response) {
-            $("#loaderdiv").hide();
-            alert("you are done");
-        }
+﻿$(document).ready(function () {
+    $("#btnOpen").click(function () {
+        $("#myModal").modal('show');
     })
-}
+
+    $("#addService").click(function () {
+        var data = $("#myform").serialize();
+        $.ajax({
+            type: "POST",
+            url: "/Service/Create",
+            data: data,
+            success: function () {
+                $('#myModal').modal('hide');
+                location.reload();
+            },
+            error: function (response) {
+                alert(response);
+            }
+
+        });
+    })
+})
+
+
+
