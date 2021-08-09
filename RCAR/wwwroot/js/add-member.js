8281396ef0b50e5cada303131c773446ad80ea27
radozip <pyrcioch.radoslaw@gmail.com>
2021-08-09 22:23:00 +0200

    Add Create script in Member Index

---
+++ RCAR/wwwroot/js/add-member.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#btnOpen").click(function () {
+        $("#myModal").modal('show');
+    })
+
+    $("#addMember").click(function () {
+        var data = $("#myform").serialize();
+        $.ajax({
+            type: "POST",
+            url: "/Member/Create",
+            data: data,
+            success: function () {
+                $('#myModal').modal('hide');
+                location.reload();
+            },
+            error: function (response) {
+                alert(response);
+            }
+
+        });
+    })
+})
+
+
+
