61b1e256da0cce62d26da0f687ae5febefa17d55
radozip <pyrcioch.radoslaw@gmail.com>
2021-08-25 10:57:05 +0200

    Add Datatable in Index Service

---
+++ RCAR/wwwroot/js/datatables.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#myTable tfoot th').each(function () {
+        var title = $(this).text();
+        if (title.trim() === 'Opcje')
+            $(this).html('');
+        else
+            $(this).html('<input type="text" style="width: 100%;" class = "form-control" placeholder=' + title + ' />');
+    });
+
+
+    $('#myTable').DataTable( {
+        "order": [[ 6, "desc" ]]
+    });
+
+
+});
