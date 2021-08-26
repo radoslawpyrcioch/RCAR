8eec14300750e4c57c4b1b0deef13871d538fe61
radozip <pyrcioch.radoslaw@gmail.com>
2021-08-26 22:54:50 +0200

    Change layout view

---
+++ RCAR/wwwroot/js/layout/layout-navsidebar.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".sidebar-btn").click(function () {
+        $(".wrapper").toggleClass("collapse");
+    });
+});
+
+function menuToggle() {
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active');
+}
