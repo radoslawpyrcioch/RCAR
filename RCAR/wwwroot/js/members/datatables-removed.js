c5db0259bd2e8563d000b7e92beb2a13ee7277e5
radozip <pyrcioch.radoslaw@gmail.com>
2021-08-30 22:43:02 +0200

    Add datatable in Removed Member, change view in Removed, Detail and Edit Member

---
+++ RCAR/wwwroot/js/members/datatables-removed.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#myTable').DataTable({
+        "order": [[2, "desc"]],
+        "language": {
+            "lengthMenu": "Wyświetl _MENU_ pozycji na stronie",
+            "zeroRecords": "Nic nie znaleziono - przykro nam",
+            "info": "Strona _PAGE_ z _PAGES_",
+            "infoEmpty": "Brak serwisów",
+            "infoFiltered": "(wyfiltrowano z _MAX_ wszystkich pozycji)",
+            "loadingRecords": "Wczytywanie...",
+            "search": "Szukaj:",
+            "paginate": {
+                "first": "Pierwsza",
+                "previous": "Poprzednia",
+                "next": "Następna",
+                "last": "Ostatnia"
+            },
+        }
+    });
+
+
+});
