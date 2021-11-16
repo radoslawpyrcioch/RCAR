dadcdab47dfc973c49cfe8d74657c9c2827fa058
radozip <pyrcioch.radoslaw@gmail.com>
2021-11-16 20:15:21 +0100

    Change in DetailService view

--- RCAR/Models/ServiceVM/ServiceDetailVM.cs
+++ RCAR/Models/ServiceVM/ServiceDetailVM.cs
@@ -11,21 +11,39 @@ namespace RCAR.Models.ServiceVM
     {
         public PaymentCreateVM PaymentCreateVM { get; set; }
         public int ServiceId { get; set; }
+
+        [Display(Name = "Numer")]
         public string ServiceNo { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; } 
+        public string LastName { get; set; }
+
+        [Display(Name = "Imię i nazwisko")]
         public string FullName 
         { get
             {
                 return FirstName + " " + LastName;
             } 
         }
+
+        [Display(Name = "Telefon")]
         public string Phone { get; set; }
+
+        [Display(Name = "Data przyjęcia")]
         public DateTime ServiceSince { get; set; }
+
+        [Display(Name = "Data zakończenia")]
         public DateTime ServiceTo { get; set; }
+
+        [Display(Name = "Marka")]
         public string Brand { get; set; }
+
+        [Display(Name = "Model")]
         public string Model { get; set; }
+
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+
+        [Display(Name = "Status")]
         public string Status { get; set; }
 
         public IEnumerable<PaymentVM> PaymentRecords { get; set; }
