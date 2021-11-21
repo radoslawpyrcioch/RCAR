c9c068ae00122ff33e5b4142584ced06e87c91e7
radozip <pyrcioch.radoslaw@gmail.com>
2021-11-21 23:01:52 +0100

    Change in DetailMember view

--- RCAR/Models/MemberVM/MemberDetailVM.cs
+++ RCAR/Models/MemberVM/MemberDetailVM.cs
@@ -1,6 +1,7 @@
 ﻿using RCAR.Models.CarVM;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,13 @@ namespace RCAR.Models.MemberVM
     public class MemberDetailVM
     {
         public int MemberId { get; set; }
+
+        [Display(Name = "Numer")]
         public string MemberNo { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Imię i nazwisko")]
         public string FullName
         {
             get
@@ -19,14 +24,30 @@ namespace RCAR.Models.MemberVM
                 return FirstName + " " + LastName;
             }
         }
+
+        [Display(Name = "Telefon")]
         public string Phone { get; set; }
+
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Display(Name = "Data przyjęcia")]
         public DateTime DateJoined { get; set; }
-        public DateTime DateLeaves { get; set; } = DateTime.UtcNow;
+
+        [Display(Name = "Data zakończenia")]
+        public DateTime DateLeaves { get; set; }
+
+        [Display(Name = "Adres")]
         public string Address { get; set; }
+
+        [Display(Name = "Miasto")]
         public string City { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
         public string PostCode { get; set; }
         public bool IsRemoved { get; set; }
+
+        [Display(Name = "Status")]
         public string Status { get; set; }
         public IEnumerable<CarsVM> Cars { get; set; }
     }
