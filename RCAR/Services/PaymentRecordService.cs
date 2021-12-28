904eba516e02e53e583da36eb42ab49bf8c87506
radozip <pyrcioch.radoslaw@gmail.com>
2021-12-28 17:37:16 +0100

    Change in CreateCarPaymentAsync

--- RCAR/Services/PaymentRecordService.cs
+++ RCAR/Services/PaymentRecordService.cs
@@ -28,7 +28,6 @@ namespace RCAR.Services
             var user = await _unitOfWork.User.FindOneAsync(u => u.Id == userId);
             if (paymentVM != null)
             {
-                // var payment = _mapper.Map<PaymentCreateVM, PaymentRecord>(paymentVM);
                 if (!user.Services.Where(s => s.ServiceId == paymentVM.ServiceId && !s.IsRemoved).Any())
                 {
                     var payment = _mapper.Map<PaymentCreateVM, PaymentRecord>(paymentVM);
@@ -50,17 +49,24 @@ namespace RCAR.Services
             {
                 if (!user.Cars.Where(c => c.CarId == paymentVM.CarId && !c.IsRemoved).Any())
                 {
-                    var payment = new PaymentRecord()
-                    {
-                        Name = paymentVM.Name,
-                        Description = paymentVM.Description,
-                        Tax = CalculateTaxAmount(paymentVM.Discount, paymentVM.TotalAmount),
-                        Discount = paymentVM.Discount,
-                        TotalAmount = CalculateDiscount(paymentVM.Discount, paymentVM.TotalAmount),
-                        NetAmount = CalculateNetAmount(paymentVM.Discount, paymentVM.TotalAmount),
-                        Status = paymentVM.Status,
-                        CarId = carId
-                    };
+                    var payment = _mapper.Map<PaymentCarCreateVM, PaymentRecord>(paymentVM);
+                    payment.Tax = CalculateTaxAmount(paymentVM.Discount, paymentVM.TotalAmount);
+                    payment.TotalAmount = CalculateDiscount(paymentVM.Discount, paymentVM.TotalAmount);
+                    payment.NetAmount = CalculateNetAmount(paymentVM.Discount, paymentVM.TotalAmount);
+                    payment.CarId = carId;
+
+                    //var payment = new PaymentRecord()
+                    //{
+                    //    Name = paymentVM.Name,
+                    //    Description = paymentVM.Description,
+                    //    Tax = CalculateTaxAmount(paymentVM.Discount, paymentVM.TotalAmount),
+                    //    Discount = paymentVM.Discount,
+                    //    TotalAmount = CalculateDiscount(paymentVM.Discount, paymentVM.TotalAmount),
+                    //    NetAmount = CalculateNetAmount(paymentVM.Discount, paymentVM.TotalAmount),
+                    //    Status = paymentVM.Status,
+                    //    CarId = carId
+                    //};
+
                     _unitOfWork.PaymentRecord.Add(payment);
                 }
                 return await _unitOfWork.SaveChangesAsync();
