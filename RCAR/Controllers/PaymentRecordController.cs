abce6034d066011e79e96ff3497af333672714d5
radozip <pyrcioch.radoslaw@gmail.com>
2022-03-02 21:43:19 +0100

    Minor changes in PaymentRecordController

--- RCAR/Controllers/PaymentRecordController.cs
+++ RCAR/Controllers/PaymentRecordController.cs
@@ -20,66 +20,66 @@ namespace RCAR.Controllers
             _paymentRecordService = paymentRecordService;
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
-            var model = new PaymentCreateVM();
-            return View(model);
+            return View(new PaymentCreateVM());
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(PaymentCreateVM paymentCreateVM, int id)
         {
             var currentUserId = User.Claims.ElementAt(0).Value;
-            var paymentCreate = await _paymentRecordService.CreatePaymentAsync(paymentCreateVM, id, currentUserId);
-            if (paymentCreate)
-                return RedirectToAction("Index", "Service");
-            else
-                return BadRequest("Brak możliwości dodania płatności");
+            if (ModelState.IsValid)
+            {
+                var paymentCreate = await _paymentRecordService.CreatePaymentAsync(paymentCreateVM, id, currentUserId);
+                if (paymentCreate)
+                    return RedirectToAction("Index", "Service");
+                else
+                    return BadRequest("Brak możliwości dodania płatności");
+            }
+            return View();
         }
 
+        [HttpGet]
         public IActionResult CreateCar()
         {
-            var model = new PaymentCarCreateVM();
-            return View(model);
+            return View(new PaymentCarCreateVM());
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateCar(PaymentCarCreateVM paymentCarCreateVM, int id)
         {
             var currentUserId = User.Claims.ElementAt(0).Value;
-            var paymentCreate = await _paymentRecordService.CreateCarPaymentAsync(paymentCarCreateVM, id, currentUserId);
-            if (paymentCreate)
-                return RedirectToAction("Index", "Member");
-            else
-                return BadRequest("Brak możliwości dodania płatności");
+            if (ModelState.IsValid)
+            {
+                var paymentCreate = await _paymentRecordService.CreateCarPaymentAsync(paymentCarCreateVM, id, currentUserId);
+                if (paymentCreate)
+                    return RedirectToAction("Index", "Member");
+                else
+                    return BadRequest("Brak możliwości dodania płatności");
+            }
+            return View();
         }
 
         public async Task<IActionResult> Delete(int id)
         {
             var currentUserId = User.Claims.ElementAt(0).Value;
-            if(ModelState.IsValid)
-            {
                 var result = await _paymentRecordService.RemovePaymentAsync(id, currentUserId);
                 if (result)
                     return RedirectToAction("Index", "Service");
                 ModelState.AddModelError("", "Niestety nie udało się usunąć.");
-            }
             return View();
         }
 
         public async Task<IActionResult> Paid(int id)
         {
             var currentUserId = User.Claims.ElementAt(0).Value;
-            if(ModelState.IsValid)
-            {
                 var result = await _paymentRecordService.PaidPaymentAsync(id, currentUserId);
                 if (result)
                     return RedirectToAction("Index", "Service");
                 ModelState.AddModelError("", "Niestety nie udało się zmienić statusu.");
-            }
             return View();
         }
-
-
     }
 }
